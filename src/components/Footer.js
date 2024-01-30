7ef8f8f0ab92342d36953ba1f6b19a5145281ef2
tomkeyes-attachdigital <tom.keyes@attachdigital.co.uk>
2024-01-30 15:10:02 +0000

    Add Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,61 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Container } from './ui';
+import { brandColours, brandFonts, minBreakpointQuery } from '../styles';
 
 const StyledFooter = styled.footer`
-  margin: 20px 0;
+  height: 120px;
+  background: ${brandColours.primary};
+  display: flex;
+  justify-items: space-between;
+`;
 
-  p {
+const StyledInner = styled.div`
+  height: 120px;
+  font-family: ${brandFonts.sans};
+  font-size: 14px;
+  color: ${brandColours.tertiary};
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 20px;
+  
+  ${minBreakpointQuery.medium`
+    flex-direction: row;
+    justify-content: space-between;
+    gap: 0px;
+  `}
+`;
+
+const StyledFooterInfo = styled.div`
     text-align: center;
-  }
+`;
+
+const StyledLink = styled.a`
+  cursor: pointer;
+  &:hover {
+    text-decoration: underline;
+   }
 `;
 
 const Footer = () => (
   <StyledFooter>
-    <p>&copy; {new Date().getFullYear()}</p>
+    <Container>
+      <StyledInner>
+        <StyledFooterInfo>
+          Diversity Travel Ltd. Registered in England and Wales. Registered Number: 05993481
+        </StyledFooterInfo>
+        <StyledLink 
+          href="https://www.attachdigital.co.uk"
+          target="_blank"
+          rel="noopener"
+        >
+          Website by Attach Digital
+        </StyledLink>
+      </StyledInner>
+    </Container>
   </StyledFooter>
 );
 
