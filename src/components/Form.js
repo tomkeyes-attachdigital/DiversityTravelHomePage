d3b44700829a86455b40ebba5aed304772ec94d4
tomkeyes-attachdigital <tom.keyes@attachdigital.co.uk>
2024-01-31 15:25:33 +0000

    make Form responsive

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,30 +1,64 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Container } from './ui';
-import { brandColours, brandFonts, sectionMargins } from '../styles';
+import { brandColours, brandFonts, sectionMargins, minBreakpointQuery } from '../styles';
 
 const StyledContactForm = styled.section`
   ${sectionMargins()};
 `;
 
+const StyledOuter = styled.section`
+  min-width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
 const StyledInner = styled.section`
   background: ${brandColours.primary};
-  padding-top: 82px;
-  padding-bottom: 87px;
-  padding-left: 101px;
-  padding-right: 101px;
   border-radius: 10px;
-`;
-const StyledForm = styled.form`
+  width: 100%;
+
+  ${minBreakpointQuery.mlarge`
+     max-width: 980px;
+  `}
 `;
 
 const StyledHeader = styled.h2`
-    font-family: ${brandFonts.serif};
-    font-size: 40px;
-    font-weight: 700;
-    color: #d3f0c8;
-    line-height: 55px;
+  font-family: ${brandFonts.serif};
+  font-size: 40px;
+  font-weight: 700;
+  color: ${brandColours.senary};
+  line-height: 55px;
+  padding: 20px;
+
+  ${minBreakpointQuery.medium`
+    padding-top: 82px;
     margin-bottom: 43px;
+    margin-left: 100px;
+    margin-right: 100px;
+  `}
+`;
+    
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-items: center;
+  border-radius: 10px;
+  margin: 20px;
+
+  ${minBreakpointQuery.medium`
+      margin-left: 100px;
+      margin-right: 100px;
+    `}
+`;
+
+const StyledName = styled.div`
+
+    ${minBreakpointQuery.medium`
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      gap: 12px;
+    `}
 `;
 
 const StyledLabel = styled.label`
@@ -36,14 +70,16 @@ const StyledLabel = styled.label`
     display: flex;
     flex-direction: column;
     flex-wrap: wrap;
+    margin-bottom: 20px;
 `;
 
 const StyledTextInput = styled.input`
-    border-radius: 20px;
+    border-radius: 30px;
     padding: 10px;
     margin: 5px;
     background: ${brandColours.tertiary};
     border: none;
+    height: 60px;
 `;
 
 const StyledTextArea = styled.textarea`
@@ -52,50 +88,66 @@ const StyledTextArea = styled.textarea`
     margin: 5px;
     background: ${brandColours.tertiary};
     border: none;
+    margin-bottom: 20px;
 `;
 
 const StyledButton = styled.button`
-    border-radius: 40px;
-    padding: 10px;
-    margin-top: 35px;
-    background: ${brandColours.senary};
-    width: 221px;
-    height: 65px;
+    background-color: ${brandColours.senary};
     font-family: ${brandFonts.sans};
+    color: ${brandColours.primary};
     font-size: 16px;
     font-weight: 700;
     line-height: 20px;
-    color: ${brandColours.primary};
+    border-radius: 40px;
+    border: none;
+    width: 221px;
+    height: 65px;
+    margin-bottom: 20px;
+    
+    ${minBreakpointQuery.medium`
+      margin-bottom: 87px;
+    `}
+
+    transition: background-color 0.2s linear;
+
+    &:hover {
+      background-color: ${brandColours.septenary};
+    };
 `;
 
 const ContactForm = () => {
   return (
     <StyledContactForm>
       <Container>
-        <StyledInner>
-          <StyledHeader>Contact Us</StyledHeader>
-          <StyledForm>
-            <StyledLabel>
-              First Name
-              <StyledTextInput type="text" name="firstName" id="firstName" />
-            </StyledLabel>
-            <StyledLabel>
-              Second Name
-              <StyledTextInput type="text" name="secondName" id="secondName" />
-            </StyledLabel>
-            <StyledLabel>
-              Email address
-              <StyledTextInput type="email" name="email" id="email" />
-            </StyledLabel>
-            <StyledLabel>
-              Message
-              <StyledTextArea type="message" name="message" rows="4" />
-            </StyledLabel>
-            <StyledButton type="submit" value="Submit">
-              Submit
-            </StyledButton>
-          </StyledForm>
-        </StyledInner>
+        <StyledOuter>
+          <StyledInner>
+            <StyledHeader>Contact Us</StyledHeader>
+            <StyledForm>
+              <StyledName>
+                <StyledLabel>
+                  First Name
+                  <StyledTextInput 
+                  type="text" name="firstName" id="firstName" />
+                </StyledLabel>
+                <StyledLabel>
+                  Second Name
+                  <StyledTextInput type="text" name="secondName" id="secondName" />
+                </StyledLabel>
+              </StyledName>
+                <StyledLabel>
+                  Email address
+                  <StyledTextInput type="email" name="email" id="email" />
+                </StyledLabel>
+                <StyledLabel>
+                  Message
+                  <StyledTextArea type="message" name="message" rows="4" />
+                </StyledLabel>
+                <StyledButton type="submit" value="Submit">
+                  Submit
+                </StyledButton>
+            </StyledForm>
+          </StyledInner>
+        </StyledOuter>  
       </Container>
     </StyledContactForm>
   );
